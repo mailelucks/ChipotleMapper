327b91e7de0899990e938c9e564026caa11dd938
Maile Lucks <maile.lucks@gmail.com>
2015-09-09 00:26:22 -0700

    Setting up jQuery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,16 @@
-navigator.geolocation.getCurrentPosition(GetLocation);
-function GetLocation(location) {
-    alert(location.coords.latitude);
-    alert(location.coords.longitude);
-    alert(location.coords.accuracy);
+$(document).ready() {
+	var map;
+	function initMap() {
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: -34.397, lng: 150.644},
+	    zoom: 8
+	  });
+	}
+
+	navigator.geolocation.getCurrentPosition(GetLocation);
+	function GetLocation(location) {
+	    alert(location.coords.latitude);
+	    alert(location.coords.longitude);
+	    alert(location.coords.accuracy);
+	}
 }
